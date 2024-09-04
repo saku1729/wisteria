300199da89ed1955b23ee8a94f6613010e44b612
saku1729 <sf24731s@st.omu.ac.jp>
2024-09-04 23:06:52 +0900

    a

--- test6.c
+++ test6.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    printf("%d-%d\n", rank, size);
+    printf("%d - %d\n", rank, size);
     MPI_Finalize();
     return 0;
 }
